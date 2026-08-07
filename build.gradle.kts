33b2f05ea5b52b2d6cefd2d2a031ac27c659d04d
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-08-07 19:22:50 -0400

    build(deps): bump collections and utils pins

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ repositories {
 
 dependencies {
     // Simplified Libraries
-    api("com.github.simplified-dev:collections") { version { strictly("652c22d") } }
-    api("com.github.simplified-dev:utils") { version { strictly("7c2feb7") } }
+    api("com.github.simplified-dev:collections") { version { strictly("7699a31") } }
+    api("com.github.simplified-dev:utils") { version { strictly("036cc09") } }
 
     // JetBrains Annotations
     api(libs.annotations)
